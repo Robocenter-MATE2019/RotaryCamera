812a82e38bbc0b6897edbd797530cb03622be3b5
shevchVit <45031357+shevchVit@users.noreply.github.com>
2018-11-19 17:05:31 +1000

    Update RotaryCamera.h

--- RotaryCamera.h
+++ RotaryCamera.h
@@ -6,14 +6,15 @@
 
 class RotaryCamera {
 public:
-	RotaryCamera();
-	void init(int8_t pin);
+	RotaryCamera(int8_t pin);
+	void init();
 	void rotate(int8_t angle);
 	~RotaryCamera();
 private:
 	Timer timer;
 	uint8_t m_angle;
 	Servo m_driver;
-	uint8_t m_pinl; // Почему pinl??? 
+	uint8_t m_pin;
 	int8_t m_lastangle = 0;
+	int8_t defAngle;
 };
